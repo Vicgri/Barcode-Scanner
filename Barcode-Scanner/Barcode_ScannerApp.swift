521467f5b9d2f94a209deb56a95cb770b0b2c6fc
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-10-04 14:24:09 +0200

    Initial Commit

---
+++ Barcode-Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode-Scanner
+//
+//  Created by Victoria Grimen on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
