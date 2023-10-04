4efd76f7a791b4d10d11e0c500a5b96670ea15b7
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-10-04 15:34:16 +0200

    Connected to a controller. Error handling

---
+++ Barcode-Scanner/ScannerVC.swift
@@ -0,0 +1,9 @@
+//
+//  ScannerViewController.swift
+//  Barcode-Scanner
+//
+//  Created by Victoria Grimen on 04/10/2023.
+//
+
+import Foundation
+import AVFoundation // needed if you are going to need the camera functions
---
+++ Barcode-Scanner/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  Barcode-Scanner
+//
+//  Created by Victoria Grimen on 04/10/2023.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
